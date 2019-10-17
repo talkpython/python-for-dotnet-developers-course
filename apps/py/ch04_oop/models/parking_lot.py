3d5c09a6faa0028064d1f319c3ff0ad4f1f1c797
Michael Kennedy <mikeckennedy@gmail.com>
2019-10-17 13:45:07 -0700

    parked some cars

---
+++ apps/py/ch04_oop/models/parking_lot.py
@@ -0,0 +1,45 @@
+from typing import List, Dict, Optional
+
+from models.car import Car
+
+
+class ParkingLot:
+
+    def __init__(self, spot_names: List[str]):
+        # self.spots = dict()
+        # for n in spot_names:
+        #     self.spots[n] = None
+
+        self.spots: Dict[str, Optional[Car]] = {
+            n: None
+            for n in spot_names
+        }
+
+    @staticmethod
+    def create(spots_per_level: int, levels: int) -> "ParkingLot":
+        names = []
+        level_names = ['A', 'B', 'C', 'D', 'E', 'G']
+        for ln in level_names[:levels]:
+            for n in range(1, spots_per_level+1):
+                names.append(f'{ln}{n}')
+
+        return ParkingLot(names)
+
+    def park(self, car: Car):
+        for k, v in self.spots.items():
+            if v is None:
+                self.spots[k] = car
+                break
+
+
+
+
+
+
+
+
+
+
+
+
+
--- apps/py/ch04_oop/program.py
+++ apps/py/ch04_oop/program.py
@@ -1,13 +1,29 @@
+from pprint import pprint
 from typing import List
 
 from models.basic_car import BasicCar
 from models.car import Car
 from models.electric_car import ElectricCar
+from models.parking_lot import ParkingLot
 from models.sports_car import SportsCar
 
 
 def main():
     cars = create_cars()
+    use_cars(cars)
+    park_cars(cars)
+
+
+def park_cars(cars: List[Car]):
+    lot = ParkingLot.create(5, 3)
+
+    for c in cars:
+        lot.park(c)
+
+    # pprint(lot.spots)
+
+
+def use_cars(cars):
     for car in cars:
         print(f"{car.model_name} is electric? {car.is_electric}")
         car.drive()
@@ -18,9 +34,11 @@ def main():
 def create_cars() -> List[Car]:
     return [
         SportsCar('Corvette', 'gas', 8, 50_000),
+        SportsCar('Camaro', 'gas', 8, 40_000),
         BasicCar('Windstar', 'gas', 6, 20_000),
         ElectricCar('Tesla', 60_000),
         ElectricCar('Bolt', 40_000),
+        ElectricCar('Leaf', 30_000),
     ]
 
 
