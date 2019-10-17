2e0ed0809ccaa18883a09519db633e1190a54f5f
Michael Kennedy <mikeckennedy@gmail.com>
2019-10-17 15:37:49 -0700

    Web scraping in Python, done.

--- apps/py/ch05_pypi/scrape.py
+++ apps/py/ch05_pypi/scrape.py
@@ -1,5 +1,6 @@
 from colorama import Fore
 import httpx
+import bs4
 
 
 def main():
@@ -9,7 +10,7 @@ def main():
 
 
 def get_html(n: int) -> str:
-    print(Fore.YELLOW + f"Getting HTML for episode {n}...")
+    print(Fore.YELLOW + f"Getting HTML for episode {n}...", flush=True)
     url = f'https://talkpython.fm/{n}'
 
     resp = httpx.get(url)
@@ -18,10 +19,22 @@ def get_html(n: int) -> str:
     return resp.text
 
 
+def get_title_from_html(n: int, html: str) -> str:
+    print(Fore.CYAN + f"Getting TITLE for episode {n}...", flush=True)
+
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    header = soup.select_one('h1')
+    if not header:
+        return "MISSING"
+
+    return header.text.strip()
+
+
 def get_titles():
     for n in range(220, 230):
         html = get_html(n)
-        title = get_title_from_html(html)
+        title = get_title_from_html(n, html)
+        print(Fore.GREEN + title)
 
 
 if __name__ == '__main__':
