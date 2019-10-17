9ef1f7149c50b2c5218ae101b126d907892a6e5a
Michael Kennedy <mikeckennedy@gmail.com>
2019-10-17 15:29:18 -0700

    getting html with Python

--- apps/py/ch05_pypi/scrape.py
+++ apps/py/ch05_pypi/scrape.py
@@ -1,9 +1,27 @@
+from colorama import Fore
 import httpx
 
 
 def main():
     print("Using Python packages")
-    print(httpx.__version__)
+
+    get_titles()
+
+
+def get_html(n: int) -> str:
+    print(Fore.YELLOW + f"Getting HTML for episode {n}...")
+    url = f'https://talkpython.fm/{n}'
+
+    resp = httpx.get(url)
+    resp.raise_for_status()
+
+    return resp.text
+
+
+def get_titles():
+    for n in range(220, 230):
+        html = get_html(n)
+        title = get_title_from_html(html)
 
 
 if __name__ == '__main__':
