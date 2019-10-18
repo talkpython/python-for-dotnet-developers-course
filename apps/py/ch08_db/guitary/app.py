9826c495550d2234092ab5958327b438b10e1ac8
Michael Kennedy <mikeckennedy@gmail.com>
2019-10-18 11:09:10 -0700

    Starter Python web app for DB chapter

---
+++ apps/py/ch08_db/guitary/app.py
@@ -0,0 +1,24 @@
+import flask
+from guitary.services import catalog_service
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+
+@app.route('/guitars')
+@app.route('/guitars/<style>')
+def guitars(style: str = None):
+    guitar_list = catalog_service.all_guitars(style)
+    return flask.render_template('guitars.html', guitars=guitar_list)
+
+
+def main():
+    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
---
+++ apps/py/ch08_db/guitary/data/guitar.py
@@ -0,0 +1,7 @@
+class Guitar:
+
+    def __init__(self, name: str, price: float, img: str, style: str):
+        self.style = style
+        self.img = img
+        self.price = price
+        self.name = name
