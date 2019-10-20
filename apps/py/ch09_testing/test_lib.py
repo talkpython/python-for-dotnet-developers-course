4a066615d967903e5a284f61e5b1a512ca33b766
Michael Kennedy <mikeckennedy@gmail.com>
2019-10-19 21:34:25 -0700

    Testing without dependencies

--- apps/py/ch09_testing/test_lib.py
+++ apps/py/ch09_testing/test_lib.py
@@ -1,4 +1,8 @@
+from typing import List
+
 import lib
+import pytest_mock
+# noinspection PyUnresolvedReferences
 from test_fixtures import guitar_data
 
 
@@ -13,8 +17,23 @@ def test_something():
 #     assert all(g.style == style for g in guitars)
 
 
-def test_electric_guitars(guitar_data):
+def test_electric_guitars(guitar_data: List[lib.Guitar], mocker: pytest_mock.MockFixture):
+    mocker.patch('lib.get_guitars_from_db', autospec=True, return_value=guitar_data)
+    mocker.patch('lib.log', autospec=True)
+
     style = 'electric'
     guitars = lib.all_guitars(style)
     # sweet little generator expression
     assert all(g.style == style for g in guitars)
+
+
+def test_all_guitars(guitar_data: List[lib.Guitar], mocker: pytest_mock.MockFixture):
+    mocker.patch('lib.get_guitars_from_db', autospec=True, return_value=guitar_data)
+    mocker.patch('lib.log', autospec=True)
+
+    style = 'all'
+    guitars = lib.all_guitars(style)
+    # sweet little generator expression
+    types = {g.style for g in guitars}
+
+    assert types == {'acoustic', 'electric'}
