20460bffb64aca98f420f032518a0b84cfa9052d
Michael Kennedy <mikeckennedy@gmail.com>
2019-10-21 17:13:37 -0700

    Starter files for deployment chapter.

---
+++ apps/py/ch12_deployment/server_setup/server_setup.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+# Consider running these two commands separately
+# Do a reboot before continuing.
+apt update
+apt upgrade -y
+
+apt install zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install some OS dependencies:
+sudo apt-get install -y -q build-essential git unzip zip nload tree
+sudo apt-get install -y -q python3-pip python3-dev python3-venv
+sudo apt-get install -y -q nginx
+# for gzip support in uwsgi
+sudo apt-get install --no-install-recommends -y -q libpcre3-dev libz-dev
+
+# Stop the hackers
+sudo apt install fail2ban -y
+
+ufw allow 22
+ufw allow 80
+ufw allow 443
+ufw enable
+
+# Basic git setup
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=720000'
+
+# Be sure to put your info here:
+git config --global user.email "you@email.com"
+git config --global user.name "Your name"
+
+# Web app file structure
+mkdir /apps
+chmod 777 /apps
+mkdir /apps/logs
+mkdir /apps/logs/guitary
+mkdir /apps/logs/guitary/app_log
+cd /apps
+
+# Create a virtual env for the app.
+cd /apps
+python3 -m venv venv
+source /apps/venv/bin/activate
+pip install --upgrade pip setuptools
+pip install --upgrade httpie glances
+pip install --upgrade uwsgi
+
+
+# clone the repo:
+cd /apps
+git clone https://github.com/mikeckennedy/python-for-dotnet-developers-materials app_repo
+
+# Setup the web app:
+cd /apps/app_repo/apps/py/ch07_web/
+pip install -r requirements.txt
+
+# Copy and enable the daemon
+cp /apps/app_repo/apps/py/Ch10_deploy/config/guitary.service /etc/systemd/system/guitary.service
+
+
+systemctl start guitary
+systemctl status guitary
+systemctl enable guitary
+
+# systemctl daemon-reload
+# If you make changes to the service file
+
+# Setup the public facing server (NGINX)
+apt install nginx
+
+# CAREFUL HERE. If you are using default, maybe skip this
+rm /etc/nginx/sites-enabled/default
+
+cp /apps/app_repo/apps/py/ch12_deployment/config/guitary.nginx /etc/nginx/sites-enabled/guitary.nginx
+update-rc.d nginx enable
+service nginx restart
+
+
+# Optionally add SSL support via Let's Encrypt:
+# https://www.digitalocean.com/community/tutorials/how-to-secure-nginx-with-let-s-encrypt-on-ubuntu-18-04
+
+add-apt-repository ppa:certbot/certbot
+apt install python-certbot-nginx
+certbot --nginx -d guitary.talkpython.com
